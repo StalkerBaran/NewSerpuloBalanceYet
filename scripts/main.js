3c869d77971a82b880730d9d2b72ef78deed3fef
StalkerBaran <120944331+StalkerBaran@users.noreply.github.com>
2025-04-10 01:14:54 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,23 @@
 //dagger
 UnitTypes.dagger.weapons.get(0).reload = 31;
 UnitTypes.dagger.weapons.get(0).bullet.damage = 13;
-UnitTypes.dagger.speed = 0.6;
+UnitTypes.dagger.speed = 0.58;
 UnitTypes.dagger.health = 180;
 UnitTypes.dagger.armor = 1;
 
 //mace
-UnitTypes.dagger.weapons.get(0).reload = 31;
-UnitTypes.dagger.weapons.get(0).bullet.damage = 13;
-UnitTypes.dagger.speed = 0.6;
-UnitTypes.dagger.health = 180;
-UnitTypes.dagger.armor = 1;
+UnitTypes.mace.speed = 0.57;
+UnitTypes.mace.armor = 6;
+
+//fortress
+UnitTypes.fortress.speed = 0.51;
+
+//scepter
+UnitTypes.scepter.speed = 0.50;
+UnitTypes.scepter.armor = 13;
+UnitTypes.scepter.health = 11000;
+
+//reign
+UnitTypes.reign.speed = 0.49;
+UnitTypes.reign.armor = 21;
+UnitTypes.reign.health = 27000;
