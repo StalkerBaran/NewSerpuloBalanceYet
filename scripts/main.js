1e4c57260c2b7683db300c0e4ffe8872ec925d04
StalkerBaran <120944331+StalkerBaran@users.noreply.github.com>
2025-04-10 00:35:05 +0700

    Create main.js
    fd

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+UnitTypes.dagger.weapons.get(0).reload = 31;
+UnitTypes.dagger.weapons.get(0).bullet.damage = 13;
+UnitTypes.dagger.speed = 0.6;
+UnitTypes.dagger.health = 180;
+UnitTypes.dagger.armor = 1;
