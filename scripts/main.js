5c8da75385bb6ce0ed2a623a6142803c2b1cccb5
StalkerBaran <120944331+StalkerBaran@users.noreply.github.com>
2025-04-11 00:02:02 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -10,14 +10,46 @@ UnitTypes.mace.speed = 0.57;
 UnitTypes.mace.armor = 6;
 
 //fortress
-UnitTypes.fortress.speed = 0.51;
+UnitTypes.fortress.speed = 0.52;
 
 //scepter
-UnitTypes.scepter.speed = 0.50;
+UnitTypes.scepter.speed = 0.51;
 UnitTypes.scepter.armor = 13;
 UnitTypes.scepter.health = 11000;
+UnitTypes.scepter.weapons.get(0).bullet.lifetime = 27;
+UnitTypes.scepter.weapons.get(2).bullet.lifetime = 67;
 
 //reign
-UnitTypes.reign.speed = 0.49;
+UnitTypes.reign.speed = 0.5;
 UnitTypes.reign.armor = 21;
 UnitTypes.reign.health = 27000;
+
+
+//nova
+UnitTypes.nova.weapons.get(0).bullet.damage = 17;
+UnitTypes.nova.speed = 0.59;
+UnitTypes.nova.health = 200;
+UnitTypes.nova.armor = 3;
+UnitTypes.nova.abilities.get(0).reload = 90;
+
+//pulsar
+UnitTypes.pulsar.speed = 0.61;
+
+//quasar
+UnitTypes.quasar.speed = 0.53;
+UnitTypes.quasar.weapons.get(0).reload = 65;
+UnitTypes.quasar.health = 830;
+
+//vela
+UnitTypes.vela.health = 8900;
+UnitTypes.vela.speed = 0.64;
+UnitTypes.vela.armor = 11;
+
+//corvus (my changes)
+UnitTypes.corvus.speed = 0.29;
+UnitTypes.corvus.armor = 12;
+UnitTypes.corvus.weapons.get(0).reload = 480;
+UnitTypes.corvus.weapons.get(0).shoot.shots = 6;
+UnitTypes.corvus.weapons.get(0).shoot.shotDelay = 14;
+UnitTypes.corvus.weapons.get(0).bullet.damage = 70;
+UnitTypes.corvus.weapons.get(0).bullet.lightningDamage = 12
