e3174e596a8271d88ec412e14f03fe0a8e1ef9b2
StalkerBaran <120944331+StalkerBaran@users.noreply.github.com>
2025-04-11 20:15:02 +0700

    Update main.js
    All units finished! Untested and need some fixes later

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,10 @@
 //dagger
 UnitTypes.dagger.weapons.get(0).reload = 31;
-UnitTypes.dagger.weapons.get(0).bullet.damage = 13;
+UnitTypes.dagger.weapons.get(0).bullet.damage = 14;
 UnitTypes.dagger.speed = 0.58;
 UnitTypes.dagger.health = 180;
 UnitTypes.dagger.armor = 1;
+UnitTypes.dagger.rotateSpeed = 8;
 
 //mace
 UnitTypes.mace.speed = 0.57;
@@ -11,6 +12,7 @@ UnitTypes.mace.armor = 6;
 
 //fortress
 UnitTypes.fortress.speed = 0.52;
+UnitTypes.scepter.weapons.get(0).bullet.lifetime = 27;
 
 //scepter
 UnitTypes.scepter.speed = 0.51;
@@ -31,6 +33,7 @@ UnitTypes.nova.speed = 0.59;
 UnitTypes.nova.health = 200;
 UnitTypes.nova.armor = 3;
 UnitTypes.nova.abilities.get(0).reload = 90;
+UnitTypes.nova.rotateSpeed = 7.5;
 
 //pulsar
 UnitTypes.pulsar.speed = 0.61;
@@ -48,11 +51,128 @@ UnitTypes.vela.armor = 11;
 //corvus (my changes)
 UnitTypes.corvus.speed = 0.29;
 UnitTypes.corvus.armor = 12;
-UnitTypes.corvus.weapons.get(0).reload = 480;
+UnitTypes.corvus.weapons.get(0).reload = 510;
 UnitTypes.corvus.weapons.get(0).shoot.shots = 6;
 UnitTypes.corvus.weapons.get(0).shoot.shotDelay = 14;
 UnitTypes.corvus.weapons.get(0).bullet.damage = 70;
-UnitTypes.corvus.weapons.get(0).bullet.lightningDamage = 12
+UnitTypes.corvus.weapons.get(0).bullet.lightningDamage = 18;
 
 
-//Пофиксить скорость тсрельбы
+//crawler. Nope
+
+// Атрах. Реал
+UnitTypes.atrax.speed = 0.95;
+
+//spiroct
+UnitTypes.spiroct.speed = 0.89;
+UnitTypes.spiroct.armor = 4;
+UnitTypes.spiroct.health = 1100;
+UnitTypes.spiroct.weapons.get(2).reload = 18;
+
+//arkiyd
+UnitTypes.arkyid.speed = 0.87;
+UnitTypes.arkyid.armor = 8;
+UnitTypes.arkyid.weapons.get(5).bullet.splashDamage = 42;
+UnitTypes.arkyid.weapons.get(7).bullet.splashDamage = 75;
+
+//toxopидор
+UnitTypes.toxopid.speed = 0.93;
+UnitTypes.toxopid.health = 18000;
+UnitTypes.toxopid.weapons.get(2).bullet.damage = 130;
+UnitTypes.toxopid.weapons.get(0).bullet.damage = 170;
+
+
+//flare
+UnitTypes.flare.speed = 2.5;
+UnitTypes.flare.weapons.get(0).reload = 14;
+UnitTypes.flare.drag = 0.07;
+UnitTypes.flare.rotateSpeed = 9;
+UnitTypes.flare.damage = 6;
+UnitTypes.flare.crashDamageMultiplier = 0.75;
+
+//horizon
+UnitTypes.horizon.speed = 2.2;
+UnitTypes.horizon.health = 470;
+UnitTypes.horizon.armor = 2;
+
+//zenith
+UnitTypes.zenith.speed = 1,64;
+UnitTypes.zenith.health = 810;
+UnitTypes.zenith.armor = 3;
+UnitTypes.zenith.weapons.get(0).bullet.lifetime = 61;
+UnitTypes.zenith.crashDamageMultiplier = 0.7;
+
+//antumbra
+UnitTypes.antumbra.health = 9100;
+UnitTypes.antumbra.speed = 0.84;
+UnitTypes.antumbra.weapons.get(0).bullet.splashDamage = 71;
+UnitTypes.antumbra.weapons.get(0).bullet.splashDamageRadius = 30;
+UnitTypes.antumbra.crashDamageMultiplier = 0.9;
+
+//eclipse
+UnitTypes.eclipse.health = 23000;
+UnitTypes.eclipse.speed = 0.77;
+UnitTypes.eclipse.armor = 19;
+
+
+//mono
+UnitTypes.mono.speed = 1.7;
+
+//poly. Everything is fine
+
+//mega
+UnitTypes.mega.speed = 2.54;
+UnitTypes.mega.health = 510;
+UnitTypes.mega.crashDamageMultiplier = 0.8;
+
+//quad
+UnitTypes.quad.speed = 1.4;
+UnitTypes.quad.buildSpeed = 3.5;
+
+//oct
+UnitTypes.oct.speed = 0.94;
+UnitTypes.oct.armor = 18;
+UnitTypes.oct.crashDamageMultiplier = 0.7;
+
+
+//risso
+UnitTypes.risso.speed = 1.06;
+UnitTypes.risso.weapons.get(0).bullet.lifetime = 50;
+
+//minke
+UnitTypes.minke.speed = 0.84;
+UnitTypes.minke.weapons.get(2).bullet.lifetime = 68;
+
+
+//bryde
+UnitTypes.bryde.speed = 0.79;
+UnitTypes.bryde.weapons.get(0).bullet.lifetime = 79;
+
+//sei
+UnitTypes.sei.speed = 0.77;
+UnitTypes.sei.weapons.get(1).bullet.lifetime = 58;
+UnitTypes.sei.weapons.get(2).bullet.lifetime = 33;
+UnitTypes.sei.armor = 13;
+
+//omura
+UnitTypes.omura.speed = 0.42;
+UnitTypes.omura.weapons.get(0).bullet.length = 475;
+
+
+//retusa. Its fine
+
+//oxynoe
+UnitTypes.oxynoe.weapons.get(0).reload = 18
+
+//cyerce
+UnitTypes.cyerce.speed = 0.795;
+UnitTypes.cyerce.health = 810;
+UnitTypes.cyerce.weapons.get(0).repairSpeed = 0.57;
+
+//aegires 
+UnitTypes.aegires.speed = 0.76;
+UnitTypes.aegires.armor = 11;
+
+//navanax
+UnitTypes.aegires.speed = 0.43;
+UnitTypes.aegires.health = 26000;
