34c7506a0230612c256a83dfe169ed1ec01ca85c
StalkerBaran <120944331+StalkerBaran@users.noreply.github.com>
2025-04-11 22:44:56 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -17,8 +17,8 @@ UnitTypes.fortress.speed = 0.52;
 UnitTypes.scepter.speed = 0.51;
 UnitTypes.scepter.armor = 13;
 UnitTypes.scepter.health = 11000;
-UnitTypes.scepter.weapons.get(0).bullet.lifetime = 59;
-UnitTypes.scepter.weapons.get(1).bullet.lifetime = 24;
+UnitTypes.scepter.weapons.get(1).bullet.lifetime = 59;
+UnitTypes.scepter.weapons.get(0).bullet.lifetime = 24;
 
 //reign
 UnitTypes.reign.speed = 0.5;
