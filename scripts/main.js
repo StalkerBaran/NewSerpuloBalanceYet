ee585a35f0882946dba38bcb5d9806be42abf4c0
blackberry2093 <120944331+StalkerBaran@users.noreply.github.com>
2025-06-03 19:31:47 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -13,9 +13,10 @@ UnitTypes.mace.rotateSpeed = 5.2; //mace
 
 UnitTypes.fortress.speed = 0.62;
 UnitTypes.fortress.health = 920;
+UnitTypes.fortress.armor = 10;
 UnitTypes.fortress.rotateSpeed = 3.4;
 UnitTypes.fortress.weapons.get(0).bullet.speed = 2.5;
-UnitTypes.fortress.weapons.get(0).bullet.lifetime = 95.7; //fortress
+UnitTypes.fortress.weapons.get(0).bullet.lifetime = 95.9; //fortress
 
 UnitTypes.scepter.speed = 0.63;
 UnitTypes.scepter.armor = 12;
